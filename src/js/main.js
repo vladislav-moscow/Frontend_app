b4170099d7beeb49c6fd44e0486d8039f445509a
Vladislav <vladislav_lebedkin@mail.ru>
2023-10-23 21:34:54 +0300

    добавил логику на активную ссылку и табы

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 const navLink = document.querySelectorAll(".nav__link");
 const header = document.getElementById("header");
+const sections = document.querySelectorAll("section[id]");
+const accordionItems = document.querySelectorAll(".questions__item");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.add("show-menu");
@@ -22,3 +24,50 @@ function scrollHeader() {
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".questions__content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
